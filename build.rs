42cb62fffad2d88abae71d234874042b6c8ddfa7
Alan WANG <alan1995wang@outlook.com>
2023-09-02 05:32:20 +0800

    fix: cmake issue

--- build.rs
+++ build.rs
@@ -1,6 +1,7 @@
 use cmake::Config;
 use std::env;
 
+
 fn main() {
     let cuda_enabled = env::var("CARGO_FEATURE_CUDA").is_ok();
     println!("cargo:warning=CUDA Status:{}", cuda_enabled);
@@ -13,10 +14,22 @@ fn main() {
         _ => "Debug",
     };
 
-    let dst = Config::new("libfam")
-        .define("CMAKE_BUILD_TYPE", profile)
-        .define("DG_TEST", "OFF")
-        .build();
-    println!("cargo:rustc-link-search=native={}", dst.display());
-    println!("cargo:rustc-link-lib=static=foo");
+    cc::Build::new()
+        .cuda(true)
+        .opt_level(3)
+        .flag("-cudart=shared")
+        .file("libfam/src/base.cu")
+        .file("libfam/src/stub.cu")
+        .file("libfam/src/arithmetic/msm.cu")
+        .compile("libfam.a");
+
+
+    // let dst = Config::new("libfam")
+    //     .define("CMAKE_BUILD_TYPE", profile)
+    //     .define("DG_TEST", "OFF")
+    //     .build();
+    // println!("cargo:rustc-link-search=native={}/lib", dst.display());
+    // println!("cargo:rustc-link-lib=static=fam");
+    println!("cargo:rustc-link-search=native=/opt/cuda/lib64/stubs");
+    println!("cargo:rustc-link-lib=cuda");
 }
