522ee172f68714ddcceec39aa0d8aa062389a351
Alan WANG <alan1995wang@outlook.com>
2023-09-05 08:33:26 +0800

    feat: save

--- src/arithmetic.rs
+++ src/arithmetic.rs
@@ -155,12 +155,12 @@ pub fn best_multiexp<C: CurveAffine>(coeffs: &[C::Scalar], bases: &[C]) -> C::Cu
         }
     }
 
-    #[cfg(feature = "cuda")]
-    impl Functor<G1Affine> for () {
-        fn invoke(coeffs: &[Fr], bases: &[G1Affine]) -> G1 {
-            cuda::msm(coeffs, bases)
-        }
-    }
+    // #[cfg(feature = "cuda")]
+    // impl Functor<G1Affine> for () {
+    //     fn invoke(coeffs: &[Fr], bases: &[G1Affine]) -> G1 {
+    //         cuda::msm(coeffs, bases)
+    //     }
+    // }
 
     <() as Functor<C>>::invoke(coeffs, bases)
 }
